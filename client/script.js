d26fc09158f201a9b53fdf92e3f5205e5d9390d3
Marte De Leeuw <martedeleeuw@hotmail.com>
2021-04-21 11:15:01 +0200

    added private messaging and disconnect

--- client/script.js
+++ client/script.js
@@ -1,17 +1,79 @@
+class User {
+    constructor(username, fontColor, socketId) {
+        this.username = username;
+        this.fontColor = fontColor;
+        this.socketId = socketId;
+    }
+}
+
 let socket = io.connect();
+let username = prompt("What is your username?");
+let fontColor = prompt("Which font-color do you want to use?");
+
+let user = new User(username, fontColor, 0);
+
+socket.on('connect', () => {
+    user.socketId = socket.id;
+})
+
+socket.emit("new user", user);
+
+socket.on("user list", users => {
+    document.getElementById('users').innerHTML = "";
+    document.getElementById('receiver').innerHTML = "";
+
+    users.forEach(user => {
+        // dropdown of users to select for a private message
+        let option = document.createElement('option');
+        option.value = user.username;
+        option.textContent = user.username;
+        document.getElementById('receiver').appendChild(option);
+
+        // list of online users
+        let listItem = document.createElement('li');
+        listItem.id = user.username;
+        listItem.classList.add('user');
+        listItem.textContent = user.username;
+        listItem.style.color = user.fontColor;
+        document.getElementById('users').appendChild(listItem);
+    })
+})
 
 document.getElementById("sendToAll").addEventListener("click", () => {
     let message = document.getElementById("message").value;
-    socket.emit('send message to everyone', message);
-});
-
-socket.on('display message', message => {
-    let listItem = document.createElement('li');
-    listItem.textContent = message;
-    document.getElementById("target").appendChild(listItem);
+    socket.emit('send message to everyone', user, message);
 });
 
 document.getElementById("sendToMyself").addEventListener("click", () => {
     let message = document.getElementById("message").value;
-    socket.emit('send message to myself', message);
+    socket.emit('send message to myself', user, message);
 });
+
+document.getElementById("sendPrivateMessage").addEventListener("click", () => {
+    let message = document.getElementById("privateMessage").value;
+    let receiverName = document.getElementById("receiver").value;
+    socket.emit('send private message', user.username, receiverName, message);
+});
+
+socket.on('display message', (user, message) => {
+    let listItem = document.createElement('li');
+    listItem.textContent = user.username + ": " + message;
+    listItem.style.color = user.fontColor;
+    document.getElementById("target").appendChild(listItem);
+});
+
+socket.on('display private message of friend', (senderName, message) => {
+    document.getElementById('receiver').value = senderName;
+    let paragraph = document.createElement('p');
+    paragraph.classList.add('messageReceived');
+    paragraph.textContent = message;
+    document.getElementById("thePrivateMessages").appendChild(paragraph);
+})
+
+socket.on('display private message of myself', (senderName, message) => {
+    let paragraph = document.createElement('p');
+    paragraph.classList.add('messageSent');
+    paragraph.textContent = message;
+    document.getElementById("thePrivateMessages").appendChild(paragraph);
+    document.getElementById('privateMessage').value = "";
+})
--- server/server.js
+++ server/server.js
@@ -7,22 +7,65 @@ app.use(express.static(clientPath));
 
 const server = http.createServer(app);
 
-server.listen(8080,() => {console.log("server running on 8080");});
+server.listen(8080, () => {
+    console.log("server running on 8080");
+});
 
 const io = require('socket.io')(server);
 
-let counter = 1;
+let users = [];
 
 io.on('connection', socket => {
-    console.log(counter + 'someone connected');
-    counter++;
 
-    socket.on('send message to everyone', message => {
-        io.emit('display message', message);
+    let currentUser = null;
+
+    socket.on('disconnect', () => {
+
+        let index = users.findIndex(user => user.username === currentUser.username);
+        users.splice(index, 1);
+
+        console.log('users list after disconnect: ')
+        console.log(users);
+
+        io.emit('user list', users);
+    })
+
+
+    socket.on('new user', user => {
+
+        for (let oldUser of users) {
+            if (user.username === oldUser.username) {
+                user = null;
+                break;
+            }
+        }
+
+        if (user !== null) {
+            currentUser = user;
+            users.push(user);
+        }
+
+        console.log('user array after new user: ');
+        console.log(users);
+
+        io.emit("user list", users);
+    })
+
+    socket.on('send message to everyone', (user, message) => {
+        io.emit('display message', user, message);
     });
 
-    socket.on('send message to myself', message => {
-        socket.emit('display message', message);
+    socket.on('send message to myself', (user, message) => {
+        socket.emit('display message', user, message);
+    });
+
+    socket.on('send private message', (senderName, receiverName, message) => {
+        users.forEach(user => {
+            if(user.username === receiverName){
+                io.to(user.socketId).emit('display private message of friend', senderName, message);
+                socket.emit('display private message of myself', senderName, message);
+            }
+        })
     })
 })
 
