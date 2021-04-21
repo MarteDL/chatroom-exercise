6b69439429658424295634b320cffc030127fee7
Marte De Leeuw <martedeleeuw@hotmail.com>
2021-04-21 15:28:03 +0200

    added styling and extra divs to chatbox

--- client/script.js
+++ client/script.js
@@ -10,13 +10,13 @@ let socket = io.connect();
 let username = prompt("What is your username?");
 let fontColor = prompt("Which font-color do you want to use?");
 
-let user = new User(username, fontColor, 0);
+let userOfThisSocket = new User(username, fontColor, 0);
 
 socket.on('connect', () => {
-    user.socketId = socket.id;
+    userOfThisSocket.socketId = socket.id;
 })
 
-socket.emit("new user", user);
+socket.emit("new user", userOfThisSocket);
 
 socket.on("user list", users => {
     document.getElementById('users').innerHTML = "";
@@ -32,7 +32,7 @@ socket.on("user list", users => {
         // list of online users
         let listItem = document.createElement('li');
         listItem.id = user.username;
-        listItem.classList.add('user');
+        listItem.classList.add('person');
         listItem.textContent = user.username;
         listItem.style.color = user.fontColor;
         document.getElementById('users').appendChild(listItem);
@@ -41,24 +41,57 @@ socket.on("user list", users => {
 
 document.getElementById("sendToAll").addEventListener("click", () => {
     let message = document.getElementById("message").value;
-    socket.emit('send message to everyone', user, message);
+    document.getElementById("message").value = "";
+    socket.emit('send message to everyone', userOfThisSocket, message);
 });
 
 document.getElementById("sendToMyself").addEventListener("click", () => {
     let message = document.getElementById("message").value;
-    socket.emit('send message to myself', user, message);
+    document.getElementById("message").value = "";
+    socket.emit('send message to myself', userOfThisSocket, message);
 });
 
 document.getElementById("sendPrivateMessage").addEventListener("click", () => {
     let message = document.getElementById("privateMessage").value;
     let receiverName = document.getElementById("receiver").value;
-    socket.emit('send private message', user.username, receiverName, message);
+    socket.emit('send private message', userOfThisSocket.username, receiverName, message);
 });
 
 socket.on('display message', (user, message) => {
+
+    console.log(userOfThisSocket.username)
+    console.log('message from '+user.username);
+
+    // creating our list element and divs inside of it
     let listItem = document.createElement('li');
-    listItem.textContent = user.username + ": " + message;
-    listItem.style.color = user.fontColor;
+    let userDiv = document.createElement('div');
+    let nameDiv = document.createElement('div');
+    let avatar = document.createElement('img');
+    let messageDiv = document.createElement('div');
+
+    //filling our divs with the right content and classes
+    userDiv.classList.add('chat-avatar');
+    nameDiv.textContent = user.username;
+    nameDiv.classList.add('chat-name');
+    avatar.src = "";
+    messageDiv.textContent = message;
+    messageDiv.classList.add('chat-text');
+
+    // attaching our elements to our messagebox and giving it the right or left class
+    userDiv.appendChild(avatar);
+    userDiv.appendChild(nameDiv);
+
+    if(user.username === userOfThisSocket.username) {
+        listItem.classList.add('chat-right');
+        listItem.appendChild(messageDiv);
+        listItem.appendChild(userDiv);
+    }
+    else {
+        listItem.classList.add('chat-left');
+        listItem.appendChild(userDiv);
+        listItem.appendChild(messageDiv);
+    }
+
     document.getElementById("target").appendChild(listItem);
 });
 
